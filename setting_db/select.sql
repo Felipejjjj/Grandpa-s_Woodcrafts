f5936eca2f67e2aff74b520e6ba50ca74fab3588
Clara Alcântara <clarabpnalcantara@gmail.com>
2025-08-20 14:49:23 -0300

    :broom: cleanup: remove comments

--- setting_db/select.sql
+++ setting_db/select.sql
@@ -91,19 +91,30 @@ where a.idArtesao in (
 
 
 
----indicies---
+-- (3) INDICES SOLICITADOS
+-- 1 INDICE: PARA FACILITAR A CONSULTA QUE UTILIZA JUNÇÃO
 create index idx_itemVenda_idVenda_idProduto on itemVenda(idVenda, idProduto);
-'''facilita querie que usa junção
 
+-- JUSTIFICATIVA: sem ele, o banco precisaria realizar 'varreduras' completas na tabela ITEMVENDA
+-- para localizar os registros, o que aumenta o tempo de execução conforme o volume de dados vai aumentando.
+-- com o índice composto (IDVENDA, IDPRODUTO), o otimizador de consultas consegue acessar os registros
+-- de forma 'focada', o que garante maior desempenho e escalabilidade do sistema em cenários de alto volume de vendas e itens associados.
+
+'''
 select v.idVenda, p.nome as produto, iv.quantidade
 from venda v inner join itemVenda iv
 on v.idVenda = iv.idVenda
 inner join produto p
 on iv.idProduto = p.idProduto;
 '''
-
+-- 2 INDICE: PARA FACILITAR LOCALIZAR RAPIDAMENTE TODOS OS PRODUTOS DE UM ARTESÃO COM ESTOQUE CARO
 create index idx_produto_idArtesao ON produto(idArtesao);
-''' Facilita localizar rapidamente todos os produtos de um artesão com estoque caro.
+-- JUSTIFICATIVA: criado para otimizar consultas que relacionam artesãos e seus produtos, 
+-- permitindo localizar rapidamente todos os produtos de um mesmo artesão. sem ele, o banco precisaria 
+-- realizar varreduras completas na tabela produto para encontrar registros associados a cada artesão, 
+-- aumentando significativamente o custo de leitura conforme o número de produtos cresce.
+
+'''
 select a.idArtesao, a.nome
 from artesao a
 where a.idArtesao in (
@@ -112,8 +123,19 @@ where a.idArtesao in (
     where (p.valorUnitario*p.qtdProduto)> 1000
 );
 '''
+-- 3 INDICE: FACILITA CRIAR UM RELATORIO SOBRE O PEDIDO DO CLIENTE
 create index idx_venda_cliente_status_data on venda(idCliente, statusPedido, datahoraVenda);
-'''facilita para criar um relatorio sobre o pedido do cliente
+  
+-- JUSTIFICATIVA: criado para otimizar consultas que envolvem relatórios de vendas por cliente,
+-- status do pedido e data da venda. sem ele, o banco precisaria realizar varreduras completas na tabela venda
+-- para localizar registros correspondentes, o que aumentaria o tempo de execução em cenários com grande
+-- quantidade de pedidos cadastrados.
+-- com o índice composto (idCliente, statusPedido, datahoraVenda), o otimizador de consultas consegue acessar 
+-- os registros de forma direcionada, tornando mais eficiente a geração de relatórios que detalham a situação 
+-- dos pedidos de cada cliente. isso garante maior desempenho e escalabilidade do sistema em operações de análise
+-- e acompanhamento de vendas.
+
+'''
 select v.idVenda, c.nome as cliente, v.statusPedido, v.datahoraVenda
 from venda v inner join cliente 
 on v.idCliente = c.idCliente;
