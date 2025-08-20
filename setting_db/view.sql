fa3d6f4b7ebba85b915641ecb4eca82def829a28
Clara Alcântara <clarabpnalcantara@gmail.com>
2025-08-20 13:48:13 -0300

    :broom: cleanup: removing comments

--- setting_db/view.sql
+++ setting_db/view.sql
@@ -1,4 +1,4 @@
---view para mostrar nome, cor e preço de um tipomadeira e que permite inserção--
+-- view para mostrar nome, cor e preço de um tipomadeira e que permite inserção [INSERÇÃO]
 create or replace view vw_tipomadeira as
 	select nome, cor, valor 
 	from tipomadeira;
@@ -7,9 +7,9 @@ select * from vw_tipomadeira;
 insert into vw_tipomadeira values('Cumaru', 'Bege', 100.00);
 
 
-----view para gerar um relatorio de compra--
+-- view para gerar um relatorio de compra [ROBUSTA]
 create or replace view vw_relatorio_compra as
-	select v.idVenda,v.statusPedido, v.datahoraVenda, c.nome as "cliente", c.telefone, p.valorTotal, p.statusPagamento, pr.nome as "produto", pr.descricao, pr.valorUnitario, ar.nome as"artesão" 
+	select v.idVenda,v.statusPedido, v.datahoraVenda, c.nome as "cliente", c.telefone, p.valorTotal, p.statusPagamento, pr.nome as "produto", pr.descricao, pr.valorUnitario, ar.nome as "artesão" 
 	from venda v join cliente c on v.idcliente = c.idcliente
 	join pagamento p on v.idPagamento = p.idPagamento
 	join itemVenda iv on v.idVenda = iv.idVenda
@@ -19,7 +19,7 @@ create or replace view vw_relatorio_compra as
 select * from vw_relatorio_compra;
 
 
---view para dados relacionado a um produto--
+-- view para dados relacionado a um produto [ROBUSTA]
 create or replace view vw_inventario_produtos_madeira as
 	select pr.nome as "produto", pr.descricao, pr.valorUnitario, pr.qtdProduto, ar.nome as "artesao", tm.nome as "madeira", tm.cor, tm.origem, tm.valor as "valor_madeira"
 	from produto pr join artesao ar on pr.idArtesao = ar.idArtesao
