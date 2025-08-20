10c67479e66910cc5c74302eee3dd0458cefb3ed
oiclai <clarabpnalcantara@gmail.com>
2025-08-19 22:19:10 -0300

    :sparkles: feat: update ToDo.md to reflect task assignments and completion status; add triggers for stock management in triggers.sql

---
+++ triggers.sql
@@ -0,0 +1,69 @@
+-- triggers
+
+-- (1) TRIGGER: NÃO PERMITIR QUE UMA COMPRA SEJA REALIZADA ONDE A QUANTIDADE DO ITEM > ESTOQUE
+CREATE OR REPLACE FUNCTION prevenir_estoque_negativo()
+RETURNS TRIGGER AS $$
+DECLARE
+    estoque_atual INT;
+BEGIN
+    SELECT qtdProduto INTO estoque_atual
+    FROM produto
+    WHERE idProduto = NEW.idProduto;
+
+    IF estoque_atual < NEW.quantidade THEN
+        RAISE EXCEPTION 'Estoque insuficiente para o produto % (disponível: %, solicitado: %)',
+            NEW.idProduto, estoque_atual, NEW.quantidade;
+    END IF;
+	
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_prevenir_estoque_negativo
+BEFORE INSERT ON itemVenda
+FOR EACH ROW
+EXECUTE FUNCTION prevenir_estoque_negativo();
+
+'''
+TESTE DE ACIONAMENTO DO TRIGGER
+
+ERROR:  Estoque insuficiente para o produto 1 (disponível: 10, solicitado: 60)
+CONTEXT:  PL/pgSQL function prevenir_estoque_negativo() line 10 at RAISE 
+'''
+-- insert into itemVenda (quantidade, idVenda, idProduto) values (60, 1, 1)
+
+
+-- (2) TRIGGER: ATUALIZAR ESTOQUE QUANDO COMPRA REALIZADA
+CREATE OR REPLACE FUNCTION atualizar_estoque()
+RETURNS TRIGGER AS $$
+DECLARE
+    estoque_atual INTEGER;
+BEGIN
+    -- Atualiza o estoque do produto
+    UPDATE produto
+    SET qtdProduto = qtdProduto - NEW.quantidade
+    WHERE idProduto = NEW.idProduto
+    RETURNING qtdProduto INTO estoque_atual;
+
+    -- Só para teste: exibe o estoque atualizado
+    RAISE NOTICE 'Estoque atualizado do produto %: % unidades',
+        NEW.idProduto, estoque_atual;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_atualizar_estoque
+AFTER INSERT ON itemVenda
+FOR EACH ROW
+EXECUTE FUNCTION atualizar_estoque();
+
+'''
+TESTE DE ACIONAMENTO DO TRIGGER
+
+NOTICE:  Estoque atualizado do produto 1: 7 unidades
+'''
+
+-- insert into itemVenda (quantidade, idVenda, idProduto) values (1, 1, 1)
+
+-- (3) TRIGGER <- Felipe faz*
